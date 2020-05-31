0cd29cd4a3758bb0bd1eb016383f47ff959ee078
Nathalie Rautell <nathalierautell@Nathalies-MacBook-Pro.local>
2020-05-31 15:27:28 +0200

    added video

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -17,6 +17,12 @@ export const HomePage = () => {
     position: relative;
   `
 
+  const Video = styled.video`
+    position: absolute;
+    z-index: -1;
+    width: 100vw;
+  `
+
   return (
     <>
       <Slide background="url(./images/page1.jpg)">
@@ -25,10 +31,13 @@ export const HomePage = () => {
         <SlideButton>View</SlideButton>
         <ArrowDown />
       </Slide>
-      <Slide background="url(./images/page2.jpg)">
+      <Slide>
         <SlideHeader>Collection</SlideHeader>
         <SlideText>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Commodi illo magni veritatis aliquam odio earum quas similique vel. Reiciendis a earum voluptate ea omnis voluptates impedit, voluptatibus eaque itaque nemo?</SlideText>
         <SlideButton>View</SlideButton>
+        <Video poster="./videos/video1.png" autoPlay muted loop>
+          <source src="./videos/video1.mp4" type="video/mp4" />
+        </Video>
         <ArrowDown />
       </Slide>
       <Slide background="url(./images/page2.jpg)">
