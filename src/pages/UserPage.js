184f8a547a99f78c56664c6f24528df1b4b8bd5a
Nathalie Rautell <nathalierautell@Nathalies-MacBook-Pro.local>
2020-06-22 12:07:20 +0200

    changed user pages

--- src/pages/UserPage.js
+++ src/pages/UserPage.js
@@ -1,18 +1,8 @@
-import React, { useState, useEffect } from 'react'
-import { user, logout, getSecretMessage } from '../reducers/user'
-import { useDispatch, useSelector } from 'react-redux'
-import { Header, Paragraph } from '../lib/Text'
+import React from 'react'
+import { Header } from '../lib/Text'
 import { PageContainer, Form, SectionImage } from '../lib/Containers'
 
-const URL = 'https://rautellin-final-project-api.herokuapp.com/users';
-
 export const UserPage = () => {
-  const dispatch = useDispatch()
-  const accessToken = useSelector((store) => store.user.login.accessToken)
-  const userId = useSelector((store) => store.user.login.userId)
-  const secretMessage = useSelector((store) => store.user.login.secretMessage)
-  const errorMessage = useSelector((store) => store.user.login.errorMessage)
-
   return (
     <>
       <PageContainer>
